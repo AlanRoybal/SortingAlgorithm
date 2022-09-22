2d886119b57efc061169fd46e6412cf1c0620350
Alan Roybal <alan.roybal@student.allenisd.org>
2022-09-22 12:24:05 -0500

    combine files

--- stringsortingalgorithm.java
+++ stringsortingalgorithm.java
@@ -1,17 +1,75 @@
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
 public class stringsortingalgorithm {
 
     String names[];
     int length;
 
+
+    public class arrayMaker {
+	// to handle exceptions include throws
+	
+	// list that holds strings of a file
+	List<String> listOfStrings
+	    = new ArrayList<String>();
+       
+	// load data from file
+	FileReader fr = new FileReader("words.txt");
+       
+	// Created a string to store each character
+	// to form word
+	String s = new String();
+	char ch;
+       
+	// checking for EOF
+	while (fr.ready()) {
+	    ch = (char)fr.read();
+               
+	    // Used to specify the delimiters
+	    if (ch == '\n') {
+               
+		// Storing each string in arraylist
+		listOfStrings.add(s.toString());
+               
+		// clearing content in string
+		s = new String();
+	    }
+	    else {
+		// appending each character to string if the
+		// current character is not delimiter
+		s += ch;
+	    }
+	}
+	if (s.length() > 0) {
+           
+	    // appending last line of strings to list
+	    listOfStrings.add(s.toString());
+	}
+	// storing the data in arraylist to array
+	String[] array
+	    = listOfStrings.toArray(new String[0]);
+       
+	// printing each line of file which is stored in
+	// array
+	for (String str : array) {
+	    System.out.println(str);
+	}	
+    }
+
+    
     public static void main(String[] args) {
         stringsortingalgorithm sorter = new stringsortingalgorithm();
-        String words[] = {"zz", "aa", "cc", "hh", "bb", "ee", "ll"}; // the strings need to be sorted are put inside this array
+        String words[] = str[]
         sorter.sort(words);
 
         for (String i : words) {
             System.out.print(i);
             System.out.print("\n");
         }
+	arrayMaker.method();
     }
 
     void sort(String array[]) {
