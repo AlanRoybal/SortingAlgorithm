5fa9e8b0424b9f9c1216ca1ee07e97212c6d657d
Alan Roybal <alan.roybal@student.allenisd.org>
2022-09-25 14:20:38 -0500

    polish code

--- arrayMaker.java
+++ arrayMaker.java
@@ -3,73 +3,9 @@ import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
-
-public class stringsortingalgorithm {
-
-    String names[];
-    int length;
-    
-    stringsortingalgorithm sorter = new stringsortingalgorithm();
-    String words[] = str.clone();
-    sorter.sort(words);
-
-    for (String i : words) {
-	System.out.print(i);
-	System.out.print("\n");
-    }
-
-
-    void sort(String array[]) {
-	if (array == null || array.length == 0) {
-	    return;
-	}
-	this.names = array;
-	this.length = array.length;
-	quickSort(0, length - 1);
-    }
-
-    void quickSort(int lowerIndex, int higherIndex) {
-	int i = lowerIndex;
-	int j = higherIndex;
-	String pivot = this.names[lowerIndex + (higherIndex - lowerIndex) / 2];
-
-	while (i <= j) {
-	    while (this.names[i].compareToIgnoreCase(pivot) < 0) {
-		i++;
-	    }
-
-	    while (this.names[j].compareToIgnoreCase(pivot) > 0) {
-		j--;
-	    }
-
-	    if (i <= j) {
-		exchangeNames(i, j);
-		i++;
-		j--;
-	    }
-	}
-	//call quickSort recursively
-	if (lowerIndex < j) {
-	    quickSort(lowerIndex, j);
-	}
-	if (i < higherIndex) {
-	    quickSort(i, higherIndex);
-	}
-    }
-
-    void exchangeNames(int i, int j) {
-	String temp = this.names[i];
-	this.names[i] = this.names[j];
-	this.names[j] = temp;
-    }
-}
-
-
 public class arrayMaker {
     // to handle exceptions include throws
-    public static void main(String[] args)
-        throws IOException
-    {
+    public static void main(String[] args) throws IOException {
         // list that holds strings of a file
         List<String> listOfStrings
             = new ArrayList<String>();
@@ -107,13 +43,7 @@ public class arrayMaker {
             listOfStrings.add(s.toString());
         }
         // storing the data in arraylist to array
-        String[] array
+        String[] array1
             = listOfStrings.toArray(new String[0]);
-       
-        // printing each line of file which is stored in
-        // array
-        for (String str : array) {
-            System.out.println(str);
-        }
     }
 }
