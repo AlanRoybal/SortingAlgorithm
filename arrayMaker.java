3336368a848ec05f754c6c1725b01530b1ec9db5
AlanRoybal <70545295+AlanRoybal@users.noreply.github.com>
2022-09-25 14:31:04 -0500

    Update arrayMaker.java

--- arrayMaker.java
+++ arrayMaker.java
@@ -4,6 +4,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class arrayMaker {
+    
+    String names[];
+    int length;
+    
     // to handle exceptions include throws
     public static void main(String[] args) throws IOException {
         // list that holds strings of a file
@@ -45,5 +49,58 @@ public class arrayMaker {
         // storing the data in arraylist to array
         String[] array1
             = listOfStrings.toArray(new String[0]);
+        
+        stringsortingalgorithm sorter = new stringsortingalgorithm();
+        String words[] = {"zz", "aa", "cc", "hh", "bb", "ee", "ll"};
+	    sorter.sort(words);
+
+        for (String i : words) {
+            System.out.print(i);
+            System.out.print("\n");
+        }
+    }
+    
+    void sort(String array[]) {
+        if (array == null || array.length == 0) {
+            return;
+        }
+        this.names = array;
+        this.length = array.length;
+        quickSort(0, length - 1);
+    }
+
+    void quickSort(int lowerIndex, int higherIndex) {
+        int i = lowerIndex;
+        int j = higherIndex;
+        String pivot = this.names[lowerIndex + (higherIndex - lowerIndex) / 2];
+
+        while (i <= j) {
+            while (this.names[i].compareToIgnoreCase(pivot) < 0) {
+                i++;
+            }
+
+            while (this.names[j].compareToIgnoreCase(pivot) > 0) {
+                j--;
+            }
+
+            if (i <= j) {
+                exchangeNames(i, j);
+                i++;
+                j--;
+            }
+        }
+        //call quickSort recursively
+        if (lowerIndex < j) {
+            quickSort(lowerIndex, j);
+        }
+        if (i < higherIndex) {
+            quickSort(i, higherIndex);
+        }
+    }
+
+    void exchangeNames(int i, int j) {
+        String temp = this.names[i];
+        this.names[i] = this.names[j];
+        this.names[j] = temp;
     }
 }
